fb2d152e2e7deffdda4fa92b50674eac031e7348
AfifAbroory <afifabroory@gmail.com>
2020-06-25 11:50:01 +0800

    Reduce Repetitive Code

--- Hangman.py
+++ Hangman.py
@@ -1,291 +1,224 @@
 from random import choice
-from time import sleep, strftime
-from os import getlogin
+from time import sleep
+from os import system, name
+
+def RandomWord():
+	ListWords = [
+	'Shark', 'Sloth', 'Hamster', 'Bee', 'Monkey', 'Wolf', 'Crab', 'Lion', 'Ant', 'Camel', 'Chicken', 
+	'Cheetah', 'Cow', 'Deer', 'Dog', 'Dolphin', 'Duck', 'Goat', 'Eagle', 'Seal', 'Bear', 'Spider', 
+	'Hornet', 'Jaguar', 'Jellyfish', 'Parrot', 'Llama', 'Giraffe', 'Mice', 'Puma', 'Zebra', 'Rabbit', 
+	'Snake', 'Fish', 'Bird', 'Tapir', 'Turtle', 'Bat', 'Wolves'
+	]
+
+	Word = choice(ListWords).upper()
+
+	return Word
+
+def HideWord(RandomWords):
+	HideWords = '_' * len(RandomWords)
+	HideWords = ' '.join(HideWords)
+
+	return HideWords
+
+def ASCII(Indx = 6):
+	Hangman = ['''
+               +---+ Chance = 6 left
+               |   |
+                   |
+                   |
+                   |
+                   |
+            ========''','''
+               +---+ Chance = 5 left
+               |   |
+               o   |
+                   |
+                   |
+                   |
+            ========''','''
+               +---+ Chance = 4 left
+               |   |
+               o   |
+               |   |
+                   |
+                   |
+            ========''','''
+               +---+ Chance = 3 left
+               |   |
+               o   |
+              /|   |
+                   |
+                   |
+            ========''','''
+               +---+ Chance 2 left
+               |   |
+               o   |
+              /|\  |
+                   |
+                   |
+            ========''','''
+               +---+ Chance = 1 left
+               |   |
+               o   |
+              /|\  |
+              /    |
+                   |
+            ========''','''
+              HANGED!
+
+               +---+
+               |   |
+               o   |
+              /|\  |
+              / \  |
+                   |
+            ========''']
+
+	return print(Hangman[Indx])
+
+# Wins and Lose Change only support ASCII
+def Result(UsedLatter, RandWord, HiddenWord, Chance = 1):
+	Win = '''
+ __   _____  _   _  __      _____ _  _ _ 
+ \ \ / / _ \| | | | \ \    / /_ _| \| | |
+  \ V / (_) | |_| |  \ \/\/ / | || .` |_|
+   |_| \___/ \___/    \_/\_/ |___|_|\_(_)
+                                         
+	'''
+	Lose = '''
+ __   _____  _   _   _    ___  ___ ___ _ 
+ \ \ / / _ \| | | | | |  / _ \/ __| __| |
+  \ V / (_) | |_| | | |_| (_) \__ \ _||_|
+   |_| \___/ \___/  |____\___/|___/___(_)
+                                         
+	'''
+	if HiddenWord == RandWord:
+		HiddenWord = HiddenWord.replace(' ','')
+
+		print('\n You Guess:', HiddenWord, '\n',Win)
+
+	elif Chance == 1:
+		RandWord = RandWord.replace(' ','')
+
+		UsedLatter = [', '.join(UsedLatter)]
+
+		for UsedLatter in UsedLatter:
+			UsedLatter = UsedLatter
+
+		print('\n',Lose,'\n')
+		print('Your Guess is: {}'.format(UsedLatter))
+		print('Correct Answear is: {}'.format(RandWord))
 
 
-# Hangman ASCII
-Hangman = ['''
-            +---+ Chance = 6 left
-            |   |
-                |
-                |
-                |
-                |
-         ========''','''
-            +---+ Chance = 5 left
-            |   |
-            o   |
-                |
-                |
-                |
-         ========''','''
-            +---+ Chance = 4 left
-            |   |
-            o   |
-            |   |
-                |
-                |
-         ========''','''
-            +---+ Chance = 3 left
-            |   |
-            o   |
-           /|   |
-                |
-                |
-         ========''','''
-            +---+ Chance 2 left
-            |   |
-            o   |
-           /|\  |
-                |
-                |
-         ========''','''
-            +---+ Chance = 1 left
-            |   |
-            o   |
-           /|\  |
-           /    |
-                |
-         ========''','''
+def AddLatter(UsedLatter, UserInput):
+	UsedLatter = UsedLatter.append(UserInput)
 
-           HANGED!
+	return UsedLatter
 
-            +---+
-            |   |
-            o   |
-           /|\  |
-           / \  |
-                |
-         ========''']
+def AddGuess(RandWord, UserGuess, HiddenWords):
+	for Indx, Latter in enumerate(RandWord):
+		if Latter == UserGuess:
+			HiddenWords = HiddenWords[:Indx] + Latter + HiddenWords[Indx+1:]
+	
+	return HiddenWords
 
-Title = '''
+def IsValid(UserInput, Bol = True):
+	if len(UserInput) == 1 and UserInput.isalpha() and Bol:
+		return True
 
-    ===================================================== 
+	elif UserInput == '1' and Bol:
+		pass
 
-        ╦ ╦    ╔═╗    ╔╗╔    ╔═╗    ╔╦╗    ╔═╗    ╔╗╔
-        ╠═╣    ╠═╣    ║║║    ║ ╦    ║║║    ╠═╣    ║║║
-        ╩ ╩    ╩ ╩    ╝╚╝    ╚═╝    ╩ ╩    ╩ ╩    ╝╚╝
+	elif UserInput == '0' and Bol:
+		pass
 
-    =====================================================
-    By: AfifAbroory
-    Webiste: AfifAbroory.github.io
-'''
+	elif len(UserInput) >= 1 and Bol:
+		pass
 
-Wins = '''
- ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
- █ ███ ██ ▄▄▄ ██ ██ ████ ███ █▄ ▄██ ▀██ █ ██
- █▄▀▀▀▄██ ███ ██ ██ ████ █ █ ██ ███ █ █ █▄██
- ███ ████ ▀▀▀ ██▄▀▀▄████▄▀▄▀▄█▀ ▀██ ██▄ █▀██
- ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
-'''
+	elif UserInput == '' and Bol:
+		pass
 
-Loses = '''
- ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
- █ ███ ██ ▄▄▄ ██ ██ ████ █████ ▄▄▄ ██ ▄▄▄ ██ ▄▄▄█ █
- █▄▀▀▀▄██ ███ ██ ██ ████ █████ ███ ██▄▄▄▀▀██ ▄▄▄█ █
- ███ ████ ▀▀▀ ██▄▀▀▄████ ▀▀ ██ ▀▀▀ ██ ▀▀▀ ██ ▀▀▀█▀█
- ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
-'''
+	else:
+		UserInput = ' Oops! Your Input "{}" Is Wrong! '.format(UserInput)
+		Border = '=' * len(UserInput)
 
-UserGuide = '''
-   ===================================================================
-    Rules:
-        ■ User Only Have Cahnce Guessing Word 6 Times.
-          If Chance is 0, User Lose.
+		print('\n',Border,'\n',UserInput,'\n',Border)
 
-        ■ If User Succes Guess a Hidden Word.
-          User Win The Game!
+		sleep(1)
 
-    User Input Guide:
-        ■ For Input Guessing Word, Input Only Accept One Latter 
-          (e.g., "a", "B", "z", etc) and Alphabet Only, Except "1"
-          and "0"
-          for Showing User Guide and Exit Program. 
-          Input is not Sensitive Case, So You Can Input 
-          Upper Case or Lower Case.
+def ExitProgram(Playing, Title, Lose, Win):
+    if Playing == 0:
+        ClearScreen()
+        print(Title)
 
-        ■ For Input Asking for Play or Wrong Input, Input Only 
-          Accept One Latter "Y" (YES) or "N" (NO) and Alphabet Only, 
-          Except "1" and "0" for Showing User Guide and Exit Program.
+        print('\n Okay, Maybe Next Time!\n\n Bye!')
+        sleep(2.5)
+        exit()
 
-        ■ For Exit Program While in Game Type "0"
+    elif Playing >= 1:
+        ClearScreen()
 
-    Program Info:
-        ■ ">>" Mean Input.
-        ■ Latest Win Score, Lose Score, and Time, Day, Date Playing
-          Saved in Log.txt.
-   ===================================================================
-'''
+        print(Title)
+        # User Score
+        print('''
+ Thanks For Playing!
 
+ +==========INFO===========+
+ | Playing Time  :{:>8} |
+ | Total Lose    :{:>8} |
+ | Total Win     :{:>8} |
+ +=========================+
+ 
+ Bye!'''.format(
+            Playing, Lose, Win))
 
-# Global Variable
-Playing = 0
-Win = 0
-Lose = 0
-GetPcName = getlogin()
-GetTime = strftime("%c")
+        sleep(2.5)
+        exit()
 
+def UserGuide():
+	ClearScreen()
 
-# Random Choice Words
-ListWords = ['Shark', 'Sloth', 'Hamster', 'Bee', 'Monkey', 'Wolf', 'Crab', 'Lion', 'Ant', 'Camel', 'Chicken', 'Cheetah', 'Cow', 'Deer', 'Dog', 'Dolphin', 'Duck', 'Goat', 'Eagle', 'Seal', 'Bear', 'Spider', 'Hornet', 'Jaguar', 'Jellyfish', 'Parrot', 'Llama', 'Giraffe', 'Mice', 'Puma', 'Zebra', 'Rabbit', 'Snake', 'Fish', 'Bird', 'Tapir', 'Turtle', 'Bat', 'Wolves']
+	print('''
+    =====User Guide=====================================================
+     Rules:
+         - User Only Have Chance Guessing Word 6 Times.
+           If Chance is 0, User Lose.
+ 
+         - If User Succes Guess a Hidden Word.
+           User Win The Game!
+ 
+     User Input Guide:
+         - For Input Guessing Word, Input Only Accept One Latter 
+           (e.g., "a", "B", "z", etc) and Alphabet Only, Except "1"
+           and "0"
+           for Showing User Guide and Exit Program. 
+           Input is not Sensitive Case, So Upper Case or Lower Case.
+           Is Same Meaning In This Program.
+ 
+         - For Input Asking for Play, Input Only 
+           Accept One Latter "Y" (YES) or "N" (NO), 
+           Except "1" for Showing User Guide.
+ 
+         - For Exit Program While in Game Type "0"
+ 
+         - ">>" Mean Input.        
+    ===================================================================''')
 
+	sleep(2)
 
-print(Title)
+def ClearScreen():
+    if name == 'nt':
+        system('cls')
 
-while True:
-    if Playing >= 1:
-        UserInput = str(input('\n(Type [1] for User Guide)\n >> Play Again? [Y/N]: ')).upper()
     else:
-        UserInput = str(input('\n(Type [1] for User Guide)\n >> Are You Ready To Play? [Y/N]: ')).upper()
+        system('clear')
 
-    # Play The Games
-    if UserInput == 'Y' and UserInput.isalpha():
-        Index = 0
-        Chance = 7
+def AskToPlay(Playing):
+    if Playing == 0:
+        UserInput = input('\n(Type [1] for User Guide)\n >> Are You Ready To Play? [Y/N]: ')
 
-        print('\n Let\'s Go!\n')
-
-        # Random Words
-        RandomWords = choice(ListWords).upper()
-
-        # Hiding Words
-        HideWords = '_' * len(RandomWords)
-        HideWords =  ' '.join(HideWords)
-
-        RandomWords = ' '.join(RandomWords)
-
-        UsedLatter = []
-
-        while True:
-            # Print Blank Words and Hangman
-            print(' ', Hangman[Index])
-            print('\n', HideWords)
-
-            # Get User Input Latter Guess
-            UserGuess = str(input('\n(Type [1] for User Guide), (Type [0] for Exit Program)\n >> Guess Animal Word: ').upper())
-
-            # If UserGuess Is One Latter and Alphabet
-            if len(UserGuess) == 1 and UserGuess.isalpha():
-                # Message If UserGuess Latter Already Use
-                if UserGuess in UsedLatter:
-                    print('\n Latter "{}" Already Used!'.format(UserGuess))
-                
-                # If UserGuess in Random Words 
-                elif UserGuess in RandomWords:
-                    Indx = 0
-
-                    # For Looping Checking Latter in RandomWords
-                    for Latter in RandomWords:
-                        # If Latter from RandomWords In UserGuess, Add Latter in HideWords
-                        if Latter == UserGuess:
-                            HideWords = HideWords[:Indx] + Latter + HideWords[Indx+1:]
-                        Indx += 1
-                    # Adding UserGuess UsedLatter
-                    UsedLatter.append(UserGuess)
-
-                    # Show Message and Break While Loop If All HideWords Already Used
-                    if HideWords == RandomWords:
-                        HideWords = HideWords.replace(' ','')
-                        print('\n You Guessed: ',HideWords)
-                        print('\n', Wins)
-                        Win += 1
-                        Playing += 1
-                        break
-
-                # Show Message Latter Not In Secret Word and Adding UserGuess latter in UsedLatter if UserGuess not in RandomWords, and Incerment Index for ASCII Art (Hangman) also Decerment Chance User.
-                else:
-                    UsedLatter.append(UserGuess)
-                    print('\n Latter "{}" Not In Secret Word!'.format(UserGuess))
-                    Chance -= 1
-                    Index += 1
-
-                    # If Chance Is Equal 1 Show Message Lose and break While Loop
-                    if Chance == 1:
-                        print(' ', Hangman[6])
-                        print('\n',Loses,'\n')
-
-                        UsedLatter = [', '.join(UsedLatter)]
-
-                        # Convert List to String
-                        for UsedLatter in UsedLatter:
-                            UsedLatter = UsedLatter
-
-                        # Remove Space
-                        RandomWords = RandomWords.replace(' ','')
-
-                        print('Your Guess is: {}'.format(UsedLatter))
-                        print('Correct Answear is: {}'.format(RandomWords))
-                        Lose += 1
-                        Playing += 1
-                        break
-
-            # Message if User Input not Alphabet and UserGuess len > 1
-            else:
-                if UserGuess == '1':
-                    print(UserGuide)
-
-                # Exit Program While In Game
-                elif UserGuess == '0':
-                    print('\n Thanks For Playing!')
-
-                    # User Score
-                    ScoreInfo = '''
-    +==========INFO===========+
-    | Playing Time  :{:>8} |
-    | Total Lose    :{:>8} |
-    | Total Win     :{:>8} |
-    +=========================+'''.format(Playing, Lose, Win)
-
-                    with open('Log.txt', 'w') as f:
-                        print(ScoreInfo, file=f)
-                        print('\nPC-NAME: {} - {}'.format(GetPcName, GetTime), file=f)
-                    print(ScoreInfo)
-
-                    sleep(2.5)
-
-                    exit()
-
-                else:
-                    UserGuess = ' Oops! Your Input "{}" Is Wrong! '.format(UserGuess)
-                    Border = "=" * len(UserGuess)
-                    print('\n',Border,'\n',UserGuess,'\n',Border)
-
-    # Exit Prorgam
-    elif UserInput == 'N' and UserInput.isalpha():
-        # Message If User Not Playing Before
-        if Playing == 0:
-            print('\n Okay, Maybe Next Time!')
-            sleep(1.5)
-
-            break
-
-        # Message If User Already Playing Games
-        else:
-            print('\n Thanks For Playing!')
-
-            # User Score
-            ScoreInfo = '''
-    +==========INFO===========+
-    | Playing Time  :{:>8} |
-    | Total Lose    :{:>8} |
-    | Total Win     :{:>8} |
-    +=========================+'''.format(Playing, Lose, Win)
-            with open('Log.txt', 'w') as f:
-                print(ScoreInfo, file=f)
-                print('\nPC-NAME: {} - {}'.format(GetPcName, GetTime), file=f)
-            print(ScoreInfo)
-
-            sleep(2.5)
-
-            break
-
-    # Message If UserInput Not 'Y', 'N', or Not Alphabet
     else:
-        # Show User Guide
-        if UserInput == '1':
-            print(UserGuide)
 
-        else: 
-            UserInput = ' Oops! Your Input "{}" Is Wrong! '.format(UserInput)
-            Border = '=' * len(UserInput)
-            print('\n',Border,'\n',UserInput,'\n',Border)
+        UserInput = input('\n(Type [1] for User Guide)\n >> Play Again? [Y/N]: ')
+
+    return str(UserInput.upper())
